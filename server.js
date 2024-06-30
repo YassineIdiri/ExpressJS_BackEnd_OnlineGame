9425add25045c647332b4fe419df1b061987acc9
YassineIdiri <psgya68@gmail.com>
2024-06-30 13:12:47 +0200

    Update Project

--- server.js
+++ server.js
@@ -1,106 +1,132 @@
 const express = require('express');
 const cors = require('cors');
+const { createClient } = require('@supabase/supabase-js');
+
 const app = express();
-const mysql = require('mysql');
+
+const supabaseUrl = 'https://gaadrwigaogjwzuksvvf.supabase.co';
+const supabaseKey = process.env.SUPABASE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdhYWRyd2lnYW9nand6dWtzdnZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTk3NDI5OTgsImV4cCI6MjAzNTMxODk5OH0.WnRdUq2JYkH5UxwtVNPmcFFsqswuyt7d4hJNoVmfH8c';
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 app.use(express.json());
 app.use(cors());
 
-const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "game"
-});
-
-app.post('/login', (req, res) => {
+app.post('/login', async (req, res) => {
     const { email, password } = req.body;
 
-    db.query('SELECT * FROM users WHERE Name = ? AND Password = ?', [email, password], (err, result) => {
-        if (err) {
-            return res.status(500).json({ message: 'Internal Server Error' });
-        }
-        if (result.length > 0) {
-            return res.json({ message: 'Login success' });
-        } else {
-            return res.status(401).json({ message: 'Login failed. Incorrect email or password.' });
-        }
-    });
+    const { data, error } = await supabase
+        .from('users')
+        .select('*')
+        .eq('name', email)
+        .eq('password', password);
+
+    if (error) {
+        return res.status(500).json({ message: 'Internal Server Error', error });
+    }
+    if (data.length > 0) {
+        return res.json({ message: 'Login success' });
+    } else {
+        return res.status(401).json({ message: 'Login failed. Incorrect email or password.' });
+    }
 });
 
 app.post('/register', async (req, res) => {
     const { name, password } = req.body;
 
-        db.query('INSERT INTO users (Name, Password) VALUES (?, ?)', [name, password], (err, result) => {
-            if (err) {
-                console.error('Error executing SQL query:', err);
-                return res.status(500).json({ message: 'Internal Server Error' });
-            }
-            return res.json({ message: 'Registration successful' });
-        });
+    const { error } = await supabase
+        .from('users')
+        .insert([{ name: name, password: password }]);
+
+    if (error) {
+        console.error('Error executing SQL query:', error);
+        return res.status(500).json({ message: 'Internal Server Error', error });
+    }
+    return res.json({ message: 'Registration successful' });
 });
 
-app.post('/win', (req, res) => {
+app.post('/win', async (req, res) => {
     const { username } = req.body;
 
-    console.log(db.query('SELECT COUNT(*) AS wins FROM game_infos WHERE player = ? AND result = "win"', [username], (err, result) => {
-        if (err) {
-            console.error('Error executing SQL query:', err);
-            return res.status(500).json({ message: 'Internal Server Error' });
-        }
-        return res.json({ wins: result[0].wins });
-    }));
+    const { data, error } = await supabase
+        .from('game_infos')
+        .select('*')
+        .eq('player', username)
+        .eq('result', 'win');
+
+    if (error) {
+        console.error('Error executing SQL query:', error);
+        return res.status(500).json({ message: 'Internal Server Error', error });
+    }
+
+    const wins = data.length;
+    return res.json({ wins });
 });
 
-app.post('/draw', (req, res) => {
+app.post('/draw', async (req, res) => {
     const { username } = req.body;
 
-    db.query('SELECT COUNT(*) AS draws FROM game_infos WHERE player = ? AND result = "draw"', [username], (err, result) => {
-        if (err) {
-            console.error('Error executing SQL query:', err);
-            return res.status(500).json({ message: 'Internal Server Error' });
-        }
-        return res.json({ draws: result[0].draws });
-    });
+    const { data, error } = await supabase
+        .from('game_infos')
+        .select('*')
+        .eq('player', username)
+        .eq('result', 'draw');
+
+    if (error) {
+        console.error('Error executing SQL query:', error);
+        return res.status(500).json({ message: 'Internal Server Error', error });
+    }
+
+    const draws = data.length;
+    return res.json({ draws });
 });
 
-app.post('/lose', (req, res) => {
+app.post('/lose', async (req, res) => {
     const { username } = req.body;
 
-    db.query('SELECT COUNT(*) AS losses FROM game_infos WHERE player = ? AND result = "lose"', [username], (err, result) => {
-        if (err) {
-            console.error('Error executing SQL query:', err);
-            return res.status(500).json({ message: 'Internal Server Error' });
-        }
-        return res.json({ losses: result[0].losses });
-    });
+    const { data, error } = await supabase
+        .from('game_infos')
+        .select('*')
+        .eq('player', username)
+        .eq('result', 'lose');
+
+    if (error) {
+        console.error('Error executing SQL query:', error);
+        return res.status(500).json({ message: 'Internal Server Error', error });
+    }
+
+    const losses = data.length;
+    return res.json({ losses });
 });
 
-app.post('/history', (req, res) => {
+app.post('/history', async (req, res) => {
     const { username } = req.body;
 
-    db.query('SELECT Id, result, DATE_FORMAT(date, "%Y-%m-%d %H:%i:%s") AS time, score FROM game_infos WHERE player = ? ORDER BY Id DESC', [username], (err, result) => {
-        if (err) {
-            console.error('Error executing SQL query:', err);
-            return res.status(500).json({ message: 'Internal Server Error' });
-        }
-        return res.json({ history: result });
-    });
+    const { data, error } = await supabase
+        .from('game_infos')
+        .select('id, result, date, score')
+        .eq('player', username)
+        .order('id', { ascending: false });
+
+    if (error) {
+        console.error('Error executing SQL query:', error);
+        return res.status(500).json({ message: 'Internal Server Error', error });
+    }
+    return res.json({ history: data });
 });
 
-app.post('/save-game', (req, res) => {
-    const { date, user, result,score } = req.body;
+app.post('/save-game', async (req, res) => {
+    const { date, user, result, score } = req.body;
 
-    db.query('INSERT INTO game_infos (date, player, result, score) VALUES (?, ?, ?, ?)', 
-    [date,user, result, score], (err, result) => {
-        if (err) {
-            console.error('Error executing SQL query:', err);
-            return res.status(500).json({ message: 'Internal Server Error' });
-        }
-        return res.json({ message: 'Game data saved successfully' });
-    });
+    const { error } = await supabase
+        .from('game_infos')
+        .insert([{ date, player: user, result, score }]);
+
+    if (error) {
+        console.error('Error executing SQL query:', error);
+        return res.status(500).json({ message: 'Internal Server Error', error });
+    }
+    return res.json({ message: 'Game data saved successfully' });
 });
-
 
 app.listen(8081, () => {
     console.log("Server listening on port 8081...");
